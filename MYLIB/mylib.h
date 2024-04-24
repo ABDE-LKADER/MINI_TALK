d327d24f75132b913d0844184db9e18a13531a57
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-04-24 13:08:06 +0100

    CHECK POINT <UPDATE MYLIB> ...

--- MYLIB/mylib.h
+++ MYLIB/mylib.h
@@ -24,7 +24,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-int		ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
